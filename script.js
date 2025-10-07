e6b28e11edda5540d4bdd58006873cbbd5b8e20a
zerotoheronugraha07 <nugrahahero81@gmail.com>
2025-10-07 12:55:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementById('sapaButton'):
+const tombolSapa = document.getElementById('sapabutton'):
 tombolSapa.addEventListener('click', function(){
   alert('Halo!Terimakasih sudah berkunjung!'):
 }):
